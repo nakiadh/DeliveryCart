431266907f0e13229c851e54eaec75b0e95eeb05
Nakia Hargrave <ndhargrave1@buffs.wtamu.edu>
2022-09-20 14:55:26 -0500

    Updated display names in classes

--- Models/Shopper.cs
+++ Models/Shopper.cs
@@ -5,8 +5,14 @@ namespace DeliveryCart.Models
     public class Shopper
     {
         public int ShopperID { get; set; }
+        [Display(Name = "Shopper Name")]
+        [Required]
         public string ShopperName { get; set; } = string.Empty;
+        [Display(Name = "Email")]
+        [Required]
         public string ShopperEmail { get; set; } = string.Empty;
+        [Display(Name = "Phone")]
+        [Required]
         public string ShopperPhone { get; set; } = string.Empty;
     }
 }
